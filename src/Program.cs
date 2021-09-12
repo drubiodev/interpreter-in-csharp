50b2b6cd62a27ddd914f3cd2e74e9d3185f498e1
Daniel Rubio <drubio432@gmail.com>
2021-09-12 13:47:14 -0400

    update folder structure

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var x = new Lexer("let x = \"name\";");
+            var x = new Lexer("== let x = \"name\";");
             var y = x.NextToken();
             System.Console.WriteLine(y.Literal);
             System.Console.WriteLine(y.Type);
