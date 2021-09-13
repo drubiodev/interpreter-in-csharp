f4df68a0c5c3a10ff917ed548a239bddbc7c4628
Daniel Rubio <drubio432@gmail.com>
2021-09-12 20:33:30 -0400

    initial start to parser

---
+++ src/Ast/Ast.cs
@@ -0,0 +1,53 @@
+﻿namespace monkey.Ast
+{
+    public class Program
+    {
+        IStatement[] Statements { get; set; }
+
+        public string TokenLiteral()
+        {
+            if (this.Statements.Length > 0)
+            {
+                return this.Statements[0].TokenLiteral();
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+
+    public class LetStatement
+    {
+        public Token Token { get; set; }
+        public Identifier Name { get; set; }
+        public IExpression Value { get; set; }
+
+        public void StatementNode() { }
+        public string TokenLiteral() { return this.Token.Literal; }
+    }
+
+    public class Identifier
+    {
+        Token Token { get; set; }
+        public string Value { get; set; }
+
+        public void ExpressionNode() { }
+        public string TokenLiteral() { return this.Token.Literal; }
+    }
+
+    public interface INode
+    {
+        string TokenLiteral();
+    }
+
+    public interface IStatement : INode
+    {
+        void StatementNode();
+    }
+
+    public interface IExpression : INode
+    {
+        void ExpressionNode();
+    }
+}
---
+++ src/Parser/Parser.cs
@@ -0,0 +1,29 @@
+﻿namespace monkey
+{
+    public class Parser
+    {
+        public Lexer Lexer { get; set; }
+        public Token CurrentToken { get; set; }
+        public Token PeekToken { get; set; }
+
+        public Parser(Lexer lexer)
+        {
+            this.Lexer = lexer;
+
+            // Read two tokens, so curent and peek are both set
+            this.Lexer.NextToken();
+            this.Lexer.NextToken();
+        }
+
+        public void NextToken()
+        {
+            this.CurrentToken = this.PeekToken;
+            this.PeekToken = this.Lexer.NextToken();
+        }
+
+        public Ast.Program ParseProgram()
+        {
+            return null;
+        }
+    }
+}
